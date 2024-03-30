aa7d2d0d3bf1515780cf071fc5fa58864628e77d
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-03-30 22:56:20 +0100

    add _putchar and _strlen functions files

---
+++ _strlen.c
@@ -0,0 +1,17 @@
+#include "printf.h"
+
+/**
+* _strlen - function that returns the length of a string
+* @s: string to get length
+* Return: length of the string
+*/
+
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+	return (length);
+}
